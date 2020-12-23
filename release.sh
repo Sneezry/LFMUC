e6ea758a054679eedb85d0a8e0fe45e1389595fd
Zhe Li <Sneezry@users.noreply.github.com>
2020-12-23 19:53:00 +0800

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,8 @@
+release=$1
+sudo apt install zip
+git clone https://github.com/Hekili/hekili.git Hekili
+cd Hekili
+git checkout $release
+sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1) then LFMUC("hekili:" .. keybind) end/' UI.lua
+rm -rf .git*
+cd ..

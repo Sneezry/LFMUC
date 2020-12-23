ff7883b3daaf273d67828eaea175d002fd3a16c3
Zhe Li <Sneezry@users.noreply.github.com>
2020-12-23 22:34:53 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 release=$1
-git clone https://github.com/Hekili/hekili.git Hekili
-cd Hekili
-git checkout $release
+wget -O hekili-origin.zip https://github.com/Hekili/hekili/releases/download/$release/Hekili-$release.zip
+unzip hekili-origin.zip
+cd hekili-origin/Hekili
 sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1) then LFMUC("hekili:" .. keybind) end/' UI.lua
 rm -rf .git*
-cd ..
+cd ../..
 echo "$1" > version.txt
 git config user.name Sneezry
 git config user.email lizhe@lizhe.org

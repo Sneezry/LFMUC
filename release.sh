b1c66b1e79c885dd125f70dad6b9730b44bd3d03
Zhe Li <Sneezry@users.noreply.github.com>
2020-12-23 21:25:13 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ git checkout $release
 sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1) then LFMUC("hekili:" .. keybind) end/' UI.lua
 rm -rf .git*
 cd ..
-echo $1 > version.txt
+echo "$1" > version.txt
 git add version.txt
 git commit -m "$1"
 git push

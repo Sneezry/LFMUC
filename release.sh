1e83417d62ff2152520c4e2538c5147a3eba0224
Zhe Li <Sneezry@users.noreply.github.com>
2021-01-07 23:28:48 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ release=$1
 wget -O hekili-origin.zip https://github.com/Hekili/hekili/releases/download/$release/Hekili-$release.zip
 unzip hekili-origin.zip -d hekili-origin
 cd hekili-origin/Hekili
-sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1) then local c_spell = UnitChannelInfo("player") if c_spell then LFMUC("wait") else LFMUC("hekili:" .. keybind) end end/' UI.lua
+sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1) then local c_spell = UnitChannelInfo("player") if c_spell then LFMUC("wait") else LFMUC("hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind .. ":hekili:" .. keybind) end end/' UI.lua
 cd ../..
 echo "$1" > version.txt
 git config user.name Sneezry

e50f4965cc929c71f2a703c6fbe3883c1eaa56e1
Zhe Li <Sneezry@users.noreply.github.com>
2021-02-17 17:51:03 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ release=$1
 wget -O hekili-origin.zip https://github.com/Hekili/hekili/releases/download/$release/Hekili-$release.zip
 unzip hekili-origin.zip -d hekili-origin
 cd hekili-origin/Hekili
-sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1 and LFHENABLED) then local c_spell = UnitChannelInfo("player") if c_spell then LFMUC("") else info=C_BattleNet.GetAccountInfoByID(select(3, BNGetInfo())) LFMUC("hekili:" .. keybind .. ":tag:" .. info.battleTag .. ":hekili:" .. keybind .. ":tag:" .. info.battleTag .. ":hekili:" .. keybind .. ":tag:" .. info.battleTag) end end/' UI.lua
+sed -i 's/b.Keybinding:SetText( keybind )/b.Keybinding:SetText( keybind ) if (i == 1 and LFHENABLED) then local c_spell = UnitChannelInfo("player") if c_spell and not LFMSSKIP then LFMUC("") else info=C_BattleNet.GetAccountInfoByID(select(3, BNGetInfo())) LFMUC("hekili:" .. keybind .. ":tag:" .. info.battleTag .. ":hekili:" .. keybind .. ":tag:" .. info.battleTag .. ":hekili:" .. keybind .. ":tag:" .. info.battleTag) end end/' UI.lua
 cd ../..
 echo "$1" > version.txt
 git config user.name Sneezry
